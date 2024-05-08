0c4b7b6c0fd150bc98eea8de3bebfb77537ef6d1
Saiteja2710 <129946081+Saiteja2710@users.noreply.github.com>
2024-05-08 11:23:03 +0530

    Update config.js

--- be/config.js
+++ be/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGOBD_URL: "mongodb://127.0.0.1:27017/Twitter",
+    MONGOBD_URL: "mongodb+srv://<saiteja>:<saiteja2710>@cluster0.w1aeqth.mongodb.net/",
     JWT_SECRET: "hafsfffgg72846285jjjkh54646467hh"
 }
